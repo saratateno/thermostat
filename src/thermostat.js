ae37009042f2d7ecb5a97a71366a6aa54e854fe3
Sara Tateno <sara.tateno@gmail.com>
2016-01-06 12:39:19 +0000

    Refactors setting a max temperature limit

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,27 +1,26 @@
-var Thermostat = (function() {
-  function Thermostat() {
-    this.temperature = 20;
-    this.powerSaving = true;
+function Thermostat() {
+  this.temperature = 20;
+  this.powerSaving = true;
+}
+
+Thermostat.prototype.up = function() {
+  if (this.powerSaving) {
+    return this._setMaxTemp(25);
+  } else {
+    return this._setMaxTemp(32);
   }
+};
 
-  Thermostat.prototype.up = function() {
-    if (this.powerSaving) {
-      return this.temperature >= 25 ? this.temperature = 25 : ++this.temperature;
-    } else {
-      return this.temperature >= 32 ? this.temperature = 32 : ++this.temperature;
-    }
-  };
+Thermostat.prototype.down = function() {
+  if(this.temperature > 10){
+    return --this.temperature;
+  }
+};
 
-  Thermostat.prototype.down = function() {
-    if(this.temperature > 10){
-      return --this.temperature;
-    }
-  };
+Thermostat.prototype.switchPowerSaving = function() {
+  this.powerSaving = !this.powerSaving;
+};
 
-  Thermostat.prototype.switchPowerSaving = function() {
-    this.powerSaving = !this.powerSaving;
-  };
-
-  return Thermostat;
-
-}());
+Thermostat.prototype._setMaxTemp = function(maximum) {
+  return this.temperature >= maximum ? this.temperature = maximum : ++this.temperature;
+};
