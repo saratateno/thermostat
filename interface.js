f9e96d6d6a7a280b4f40d1b84da8265781d38f97
Katie Smith <kls.smith@yahoo.co.uk>
2016-01-06 17:58:43 +0000

    Add button functionality

--- interface.js
+++ interface.js
@@ -1,9 +1,39 @@
 $( document ).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
+  updateTemperature();
+  updatePowerSavingStatus();
 
   $('#temp_up').click(function() {
     thermostat.up();
-    $('#temperature').text(thermostat.temperature);
+    updateTemperature();
   });
+
+  $('#temp_down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  $('#temp_reset').click(function() {
+    thermostat.resetTemp();
+    updateTemperature();
+  });
+
+  $('#PSM_switch').click(function() {
+    thermostat.switchPowerSaving();
+    updatePowerSavingStatus();
+    updateTemperature();
+  });
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+  }
+
+  function updatePowerSavingStatus() {
+    if (thermostat.powerSaving) {
+      $('#PSM_status').text("Power Saving is On");
+    } else {
+      $('#PSM_status').text("Power Saving is Off");
+    }
+  }
+
 });
